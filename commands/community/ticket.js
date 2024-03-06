22d80cb9332708985e3bcedca0d0cd126fa7a540
Thefonze74065 <guptalin@gmail.com>
2024-03-06 01:19:47 -0600

    Update ticket.js
    added some comments

--- commands/community/ticket.js
+++ commands/community/ticket.js
@@ -15,17 +15,20 @@ module.exports = {
 		.setDescription("Create a ticket embed and button")
 		.setDefaultMemberPermissions(PermissionsBitField.ManageChannels),
 	async execute(interaction, client) {
+		// Check if the user has the required permissions
 		if (
 			!interaction.member.permissions.has(
 				PermissionsBitField.Flags.ManageChannels
 			)
 		) {
+			// If the user does not have the required permissions, send an error message
 			return await interaction.reply({
 				content: "You do not have permission to use this command",
 				ephemeral: true,
 			});
 		}
 
+		// Create the button
 		const button = new ActionRowBuilder().addComponents(
 			new ButtonBuilder()
 				.setCustomId("ticket")
@@ -44,11 +47,13 @@ module.exports = {
 			},
 		});
 
+		// Create the embed
 		const embed = new EmbedBuilder()
 			.setColor(guildData.embedColor)
 			.setTitle("Tickets & Support")
 			.setDescription("Click the button below to create a ticket");
 
+		// Send the embed and button
 		await interaction.channel.send({ embeds: [embed], components: [button] });
 		await interaction.reply({ content: "Ticket Embed Created!", ephemeral: true });
 	},
