da1efd0a4cf388c6f5e1f15387fcfcbfd9c38a88
Thefonze74065 <guptalin@gmail.com>
2024-03-05 22:35:52 -0600

    Created Rule Command
    added dotenv-cli back in...
    because windows i guess?
    IDK why it would'nt load the variables without it.

--- commands/community/rules.js
+++ commands/community/rules.js
@@ -15,7 +15,7 @@ module.exports = {
 			subcommand.setName('add')
 				.setDescription('Add a new rule')
 				.addStringOption(option =>
-					option.setName('rule')
+					option.setName('rule-content')
 						.setDescription('The rule to add')
 						.setRequired(true)))
 		.addSubcommand(subcommand =>
@@ -24,6 +24,17 @@ module.exports = {
 				.addIntegerOption(option =>
 					option.setName('rule')
 						.setDescription('The index of the rule to delete')
+						.setRequired(true)))
+		.addSubcommand(subcommand =>
+			subcommand.setName('edit')
+				.setDescription('edit a rule')
+				.addIntegerOption(option =>
+					option.setName('rule')
+						.setDescription('The index of the rule to edit')
+						.setRequired(true))
+				.addStringOption(option =>
+					option.setName('rule-content')
+						.setDescription('The edited rule')
 						.setRequired(true))),
 	async execute(interaction, client) {
 		// Fetch the pinned message in the channel
@@ -42,7 +53,8 @@ module.exports = {
 		}
 
 		const subcommand = interaction.options.getSubcommand(); // 'add' or 'delete'
-		const rule = interaction.options.getString('rule');
+		const rule = interaction.options.getString('rule-content');
+		const ruleinteger = interaction.options.getInteger('rule');
 
 		// Fetch the content of the pinned message and split it into rules
 		const rulesEmbed = pinnedMessage.embeds[0];
@@ -51,24 +63,35 @@ module.exports = {
 		if (!rulesEmbed.description) {
 			rules = ['No rules'];
 		} else {
-			rules = rulesEmbed.description.split('\n');
+			rules = rulesEmbed.description.split('\n').map(item => item.substring(item.indexOf('. ') + 2));;
 		}
 
 		// Depending on the subcommand, add or delete a rule
 		if (subcommand === 'add') {
+			//if the list of rules is only "No rules", remove it
+			if (rules.length === 1 && rules[0] === 'No rules') {
+				rules = [];
+			}
+			// Add the new rule to the list of rules
 			rules.push(rule);
 		} else if (subcommand === 'delete') {
+			// Remove the rule from the list of rules
 			const index = rule - 1;
 			rules.splice(index, 1);
+			//if the list of rules is empty, add "No rules" to it
 			if (rules.length === 0) {
 				rules = ['No rules'];
 			}
+		} else if (subcommand === 'edit') {
+			// Edit the rule in the list of rules
+			const index = ruleinteger - 1;
+			rules.splice(index, 1, rule);
 		}
 
 		// Create an embed for the rules section
 		const newRulesEmbed = new EmbedBuilder()
 			.setTitle('Rules')
-			.setDescription(rules.join('\n'));
+			.setDescription(rules.map((item, i) => `${i + 1}. ${item}`).join('\n'))
 
 		// Edit the pinned message with the new embed
 		await pinnedMessage.edit({
